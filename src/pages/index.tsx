f7135d903f945bcea66c4dab556a9401e7cad3b7
edward <bepyan@naver.com>
2022-09-02 14:13:39 +0900

    feat. 지도 마크업

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import React from 'react';
 
 import ArrowRight from '@/components/icons/ArrowRight';
 import Separator from '@/components/icons/Separator';
@@ -8,8 +9,20 @@ import { $ } from '@/utils/core';
 export default function HomePage() {
   return (
     <Layout>
-      <div className="h-[60%]">
-        <div className="bg-gray-100"></div>
+      <div className="relative h-[60%] bg-[url('/map.png')] bg-cover bg-no-repeat">
+        <ParkPointer status="hightlight" className="top-12 left-12">
+          {117}대 보유
+        </ParkPointer>
+        <ParkPointer status="nomal" className="top-12 left-24">
+          {26}대 보유
+        </ParkPointer>
+        <ParkPointer status="disabled" className="top-24 left-12">
+          {0}대 보유
+        </ParkPointer>
+
+        <ParkPointer status="nomal" className="top-24 left-24">
+          P
+        </ParkPointer>
       </div>
 
       <div className="h-[40%] overflow-scroll border-t border-[#e2e2e2] bg-white">
@@ -20,6 +33,53 @@ export default function HomePage() {
     </Layout>
   );
 }
+
+type TParkPointerStatus = 'hightlight' | 'nomal' | 'disabled';
+
+interface ParkPointerProps {
+  className?: string;
+  status: TParkPointerStatus;
+  children?: React.ReactNode;
+}
+
+const ParkPointer = ({ className, status, children }: ParkPointerProps) => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(`/${children?.toString()}`);
+  };
+
+  return (
+    <div className={$('absolute flex flex-col', className)}>
+      <button
+        onClick={onClick}
+        disabled={status === 'disabled'}
+        className={$(
+          'rounded-lg border-2 px-3 py-2 text-center font-bold',
+          'transition-all active:opacity-90',
+          status === 'hightlight'
+            ? 'border-[#0C79FE] bg-[#0C79FE] text-white'
+            : status === 'nomal'
+            ? 'border-[#66707C] bg-white'
+            : 'border-[#999999] bg-[#dcdcdc] text-[#777777]',
+        )}
+      >
+        {children}
+      </button>
+      <svg
+        width="6"
+        height="20"
+        viewBox="0 0 6 20"
+        fill={status === 'hightlight' ? '#0C79FE' : status === 'nomal' ? '#66707C' : '#999999'}
+        xmlns="http://www.w3.org/2000/svg"
+        className="self-center"
+      >
+        <rect x="2" width="2" height="16" />
+        <circle cx="3" cy="17" r="3" />
+      </svg>
+    </div>
+  );
+};
 
 const ParkItem = () => {
   const router = useRouter();
