ffe0a1836d00292d46d278f59d49e37c5fb11fec
edward <bepyan@naver.com>
2022-09-02 18:08:00 +0900

    feat. meta 데이터 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,15 +9,25 @@ export interface LayoutProps {
 }
 
 export default function Layout({ className, children, title }: LayoutProps) {
-  const ogTitle = !title ? 'oright' : `${title} | oright`;
+  const ogTitle = !title ? '오라이 Oright' : `${title} | 오라이 Oright`;
 
   return (
     <main className={$('h-full w-full bg-[#f2f4f6]')}>
       <Head>
         <title>{ogTitle}</title>
         <meta property="og:title" key="title" content={ogTitle} />
-        <meta property="og:description" key="description" content={''} />
-        <meta property="og:image" key="image" content={''} />
+        <meta
+          property="og:description"
+          key="description"
+          content={'지금 바로 주차 공간을 확인하세요!'}
+        />
+        <meta
+          property="og:image"
+          key="image"
+          content={
+            'https://user-images.githubusercontent.com/65283190/188105441-0bb941e4-af2e-40e2-9f6a-ba33d6426b27.png'
+          }
+        />
       </Head>
 
       <div className={$('relative mx-auto h-full w-full max-w-md', className)}>{children}</div>
