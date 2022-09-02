18cdfc01cfb7f7001527bd7dbd5c26ce569363e9
edward <bepyan@naver.com>
2022-09-02 12:06:38 +0900

    feat. 주차장 디테일 페이지 - 주차 현황 표시 마크업 수정

--- src/components/icons/Separator.tsx
+++ src/components/icons/Separator.tsx
@@ -8,12 +8,12 @@ export default function Separator({ height = 12, className }: SeparatorProps) {
     <svg
       width="1"
       height={height}
-      viewBox="0 0 1 12"
+      viewBox={`0 0 1 ${height}`}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
     >
-      <rect width="1" height="12" fill="#C7CFD9" />
+      <rect width="1" height={height} fill="#C7CFD9" />
     </svg>
   );
 }
